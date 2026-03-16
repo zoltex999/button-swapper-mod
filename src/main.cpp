7d931790ed384c7ca97277964c71b3397f8cdc6a
Zoltex <zoltex0@gmail.com>
2026-03-16 22:05:41 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,39 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <string_view>
+#include <unordered_set>
+#include <vector>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayerSwap, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        auto menu = this->getChildByID("main-menu");
+        if (!menu) return true;
+
+        auto play = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("play-button"));
+        auto editor = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("editor-button"));
+
+        auto garage = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("garage-button"));
+
+        if (play && editor) {
+            auto pPos = play->getPosition();
+            auto ePos = editor->getPosition();
+
+            play->setPosition(ePos);
+            editor->setPosition(pPos);
+            play->setScale(0.65f);
+            editor->setScale(1.45f);
+        } else {
+            log::error("error while swapping buttons  >:(");
+        }
+
+        if (garage) {
+            garage->setScale(0.65f);
+        }
+
+        return true;
+    }
+};
